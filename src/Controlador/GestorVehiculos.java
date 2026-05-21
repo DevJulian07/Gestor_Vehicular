010b43316f59638edd40a34de409dc54a445a490
DevJulian07 <JulianGarcia.18@outlook.com>
2026-05-21 13:06:27 -0500

    finalizacion GestorVehiculos

--- src/Controlador/GestorVehiculos.java
+++ src/Controlador/GestorVehiculos.java
@@ -14,7 +14,20 @@ public class GestorVehiculos {
     private ArrayList<Vehiculo> listaVehiculos;
     
     public GestorVehiculos() {
-        this.listaVehiculos = new ArrayList<>();
+        this.listaVehiculos = new ArrayList<>();  
     }
     
+     public void agregarVehiculo(String pais, String marca, String tipoVehiculo, String modelo, int anioFabricacion, boolean esNuevo){
+         Vehiculo nuevoVehiculo = new Vehiculo(pais, marca, tipoVehiculo, modelo, anioFabricacion, esNuevo);
+         this.listaVehiculos.add(nuevoVehiculo);
+        }
+     public String eliminarVehiculo(int indice){
+         if (indice >=0 && indice < this.listaVehiculos.size()){
+             Vehiculo eliminado = this.listaVehiculos.get(indice);
+             this.listaVehiculos.remove(indice);
+             return "vehiculo eliminado: " + eliminado.getMarca() + "" + eliminado.getModelo();
+         } else {
+             return "Error: indice invalido.";
+         }
+     }
 }
